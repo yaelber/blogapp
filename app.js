655af2e7630be103fe73ac7d8c2dcc2d179859f3
Demo <demo@Zombie.local>
2016-08-07 13:02:04 -0400

    more

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 // include packages
 var express = require('express');
 var fs = require('fs');
+var _ = require('lodash');
 
 // create an http server
 var app = express();
 
 var getPosts = function(id) {
   var data = JSON.parse(fs.readFileSync('db.json').toString());
-  return data.posts;
+  if (id === undefined) {
+    return data.posts;
+  }
+  for (i in data.posts) {
+    if (data.posts[i].id === id) {
+      return data.posts[i];
+    }
+  }
 };
+
+// var editPost = function(post) {
+//   var oldPost = getPost(post.id);
+//   Object.assign({}, oldPost, post);
+//   var data = JSON.parse(fs.readFileSync('db.json').toString());
+//
+// };
 
 // handle incoming requests to the "/" endpoint
 app.get('/', function (request, response) {
   fs.readFile('index.html', function(error, html) {
-    response.send(html.toString());
+    var compiled = _.template(html);
+    var posts = getPosts();
+    response.send(posts);
   });
 });
 
